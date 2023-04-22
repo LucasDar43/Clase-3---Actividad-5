2e9d9d37f92e639e5021552117b1098941b9795e
lucas <lucas@DESKTOP-5TLC2TC>
2023-04-22 17:41:16 -0300

    correccion

--- Clase 3 - Actividad 5/Program.cs
+++ Clase 3 - Actividad 5/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace actividad5
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Ingrese el numero mas alto para calcular la cantidad de numeros primos en ese rango");
+            int maximo = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= maximo; i++)
+            {
+                bool esPrimo = true;
+
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+
+                if (esPrimo)
+                {
+                    Console.WriteLine(i + " es primo.");
+                }
+            }
+
+        }
+    }
+}
